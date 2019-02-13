f17b04bf8f81e6363f5a0a5febb38cb27ffbda86
tong.liu <r0692616@kuleuven.be>
2019-02-13 14:42:55 +0100

    readme changes check

---
+++ chapter2/data_type_number.dart
@@ -0,0 +1,16 @@
+/*
+*  num 只有两种类型 int 和 double
+*
+*/
+void main(){
+  num a = 10;
+  a = 10.65;
+
+  //int var cannot be assigned a double value
+  int b = 20;
+  //b = 12.5;
+
+  double c = 12.5;
+  c = 20;
+  print(c);
+}
